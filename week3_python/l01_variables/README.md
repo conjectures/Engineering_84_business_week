295642004e675bcf4b4ed3a7565d7bfb84795f6e
conjectures <alexis.othonos@gmail.com>
2021-03-22 16:04:51 +0000

    added github guide

--- week3_python/l01_variables/README.md
+++ week3_python/l01_variables/README.md
@@ -1,8 +1,10 @@
 # Python Variables
+> time: 45 minutes
 
-
-## Task
-
+## Objectives
+- Environment setup
+- Github
+- Comments
 - What is a variable
 - Dynamically typed
 - User interaction
@@ -14,7 +16,15 @@ print("Hello, world")
 ```
 If the outcome is `Hello, world`, then everything is working fine.
 
-## Using .gitignore
+### Using .gitignore
 We use the file `.gitignore` to specify the files we don't want to include in the repo
+
+### Files added to github
+The `.gitignore` file will be taken in account and the enviromnet files will not be uploaded.
+
+### Comments
+To add comments in python we use `#`
+
+### Variables
 
 
--- week3_python/l01_variables/variables.py
+++ week3_python/l01_variables/variables.py
@@ -1,2 +1,26 @@
+# pseudocoding
 
+# Testing env with hello world message
 print("Hello, world")
+
+# What is a variable?
+course_name = 'devops 84'  # string
+student_name = 'alex'  # another string
+age = 16  # int
+hourly_wage = 8.9  # float
+
+print(student_name)
+print(age)  # python will 'translate' the age integer into string when printing
+print(hourly_wage)  # same as float
+
+# We can use `input()` to get data from user:
+first_name = input('What is your name?')
+
+print(f'hello {first_name}')
+
+age = input('What is your age?')
+# Check data type of variable with type()
+print(type(age))
+
+# Change the datatype to int with int()
+print(type(int(age)))
