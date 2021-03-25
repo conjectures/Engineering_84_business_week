2d3b4e39f94c2d09b882ae22bceeff73120f48fe
conjectures <alexis.othonos@gmail.com>
2021-03-25 13:51:21 +0000

    minor changes

--- animal.py
+++ animal.py
@@ -13,6 +13,7 @@ class Animal():
         self.alive = True
         self.spine = True
         self.lungs = True
+        self.hair = False
 
     def move(self):
         return "Move a bit"
@@ -22,3 +23,11 @@ class Animal():
 
     def breath(self):
         return "Oxygen"
+
+
+# Example of an animal object
+cat = Animal()
+oriental_longhair = Animal()
+
+oriental_longhair.hair = True # Polymorphism - overwriting class values according to our needs
+print(oriental_longhair.hair)
