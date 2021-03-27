60a388d2d22efb8c9d46ccdb4f98f40cff78089d
conjectures <alexis.othonos@gmail.com>
2021-03-27 00:04:06 +0000

    task1 docs complete

--- task_01_birthday_calculation/task1.py
+++ task_01_birthday_calculation/task1.py
@@ -14,19 +14,29 @@ name = 'bob'
 
 print(f'OMG {name}, you are {age} years old, so you were born in {current_year-age}\n\n')
 
-#PART 2
+
+# PART 2
 # prompt the user for input and re-assing the variable age and name
 # figure out a way to account for if the persons bithday has already happened this year or not
 
-name = input('What is your name?')
+name = input('What is your name?\n')
 reprompt = True
 while reprompt:
-    age = input('What is your age?')
+    age = input('When were you born? (year)\n')
+    # Check if input is a number
     if age.isdigit():
         reprompt = False
     else:
-        print('Please enter a correct value')
+        print('Bad value. Try again.')
 
-# TODO
+bday = input('Did you have your birthday this year? (y/n)\n').lower()
+while bday not in ['n', 'no', 'y', 'yes']:
+    bday = input("Sorry, i didn't catch that, did you already have your birthday this year? (y/n)\n").lower()
 
+# If birthday passed, add a year to their age
+if bday in ['n', 'no']:
+    year_born = current_year - age - 1
+else:
+    year_born = current_year - age
 
+print(f'OMG {name}, you are {age} years old, so you were born in {year_born}\n\n')
