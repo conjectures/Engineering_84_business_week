6d865ac8afdaae54b7bfa06a4e43265f7f3f0bca
conjectures <alexis.othonos@gmail.com>
2021-03-27 16:15:08 +0000

    added week 3 subject 02

---
+++ week3_python/s02-userinput/README.md
@@ -0,0 +1,14 @@
+# Subject 02 - User Input
+> Time: 20'
+
+In Python, we can get user input with the keyword `input`. When called, the terminal will wait for the user to input text, until the *Enter* key is pressed.
+`input` needs to be called with parenthesies attached:
+```python
+value = input()
+```
+
+If needed, a short text message can be added to the `input` call that will convey information to the user before asking for input
+```python
+name = input('Please write your name: ')
+```
+
---
+++ week3_python/s02-userinput/user_input.py
@@ -0,0 +1,16 @@
+
+# Get user input for first name, last name, age, salary, date of birth and post code
+
+first_name = input('Please enter your first name: ')
+last_name = input('Please enter your last name: ')
+age = int(input('Please enter your age: '))
+salary = float(input('Please enter your (hourly) salary: '))
+birthday = input('Please enter your date of birth: ')
+postcode = input('Please enter your postcode: ')
+
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(salary))
+print(type(birthday))
+print(type(postcode))
