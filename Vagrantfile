278b81496c8519aa3d5b29934a5da3703c9b8e24
conjectures <alexis.othonos@gmail.com>
2021-05-06 14:22:36 +0100

    modified playbooks

--- Vagrantfile
+++ Vagrantfile
@@ -1,13 +1,14 @@
 Vagrant.configure("2") do |config|
 
-
 # Creating third VM called controller
   config.vm.define "provisioner" do |provisioner|
     provisioner.vm.box = "generic/ubuntu1804"
     provisioner.vm.hostname = 'provisioner'
     provisioner.vm.network "private_network", ip: "192.168.33.10"
     provisioner.vm.synced_folder "provision", "/home/vagrant/provision"
-    provisioner.vm.provision "shell", path: "project/entrypoint.sh"
+    provisioner.vm.synced_folder "application", "/home/vagrant/application"
+    provisioner.vm.provision "shell", path: "provision/entrypoint.sh"
+    provisioner.ssh.forward_agent = true
 
     # config.hostsupdater.aliases = ["development.controller"]
    end
--- provision/entrypoint.sh
+++ provision/entrypoint.sh
@@ -4,10 +4,21 @@ sudo apt-get install software-properties-common -y
 sudo apt-get update -y
 
 # Install packages
-sudo apt-get install ansible python3 python3-pip -y
+sudo apt-get install ansible awscli python3 python3-pip -y
 # Install required pip modules
 pip3 install boto boto3
 
 # Ansible file structure
-mkdir -r /etc/ansible/groups_vars/all
+mkdir -p /etc/ansible/group_vars/all
+sudo cp /home/vagrant/provision/pass.yaml /etc/ansible/group_vars/all/
+sudo cp /home/vagrant/provision/playbook.yaml /etc/ansible/
 
+#ansible-playbook /etc/ansible/playbook.yml -e 'ansible_python_interpreter=/usr/bin/python3'
+ansible-galaxy collection install ansible.posix
+export ANSIBLE_HOST_KEY_CHECKING=False
+export AWS_REGION=eu-west-1
+
+# ansible-playbook /etc/ansible/playbook.yaml -vvv \
+#     --ask-vault-pass \
+#     -e "ansible_python_interpreter=/usr/bin/python3" \
+#     -t ec2-create,ec2-configure
