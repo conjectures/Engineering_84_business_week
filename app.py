f4069981f71ac14fca53618b751a034750e393af
conjectures <alexis.othonos@gmail.com>
2021-06-01 01:07:21 +0100

    added to docs

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 # Create an app instance
 app = Flask(__name__)
 
+
 # Use decorator to define the route for our web page
 @app.route("/")
 # Set up default page
@@ -13,11 +14,33 @@ def index():
 
 @app.route("/welcome/")
 def welcome():
-    return "Welcome on board"
+    return """
+            <!DOCTYPE html>
+            <html lang="en">
+              <head></head>
+              <body>
+                <h1> Welcome </h1>
+                <p> This is a welcome message from the DevOps team </p>
+              </body>
+            </html>
+           """
+
 
 @app.route("/home/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/greeting")
+def greet():
+    name = 'Alex'
+    # Pass the variable 'name' to the index html page
+    return render_template("index.html", name=name)
+
+
+@app.route("/details")
+def details():
+    return render_template("details.html", name='Alex', dictionary={'name': 'Alex', 'age': 24}, list=['apple', 'banana', 'coconut', 'grapes'])
 
 
 if __name__ == "__main__":
